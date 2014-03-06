4523e4f6fb60a803c0dcd2245b4d69ed675e2059
Yiteng Zhang <ytzhang89@gmail.com>
2014-03-06 22:16:32 +0000

    Test 7
    previous test worked css file didn't load properly, so delete everything

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Connect to server and select databse.
-mysql_connect("eu-cdbr-azure-west-b.cloudapp.net", "be1a7d8c1e05ec", "9b94feea")or die("cannot connect"); 
-mysql_select_db("gamehubA9Mv7nxeB")or die("cannot select DB");
-?>
