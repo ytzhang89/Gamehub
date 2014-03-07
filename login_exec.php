392e62528debec7c5adf0c097d7edeee2f72efba
Yiteng Zhang <ytzhang89@gmail.com>
2014-03-07 16:51:10 +0000

    Test 18

--- login_exec.php
+++ login_exec.php
@@ -3,7 +3,7 @@
 	session_start();
  
 	//Include database connection details
-	require_once('connection.php');
+	include('connection.php');
  
 	//Array to store validation errors
 	$errmsg_arr = array();
