5ff01fe3d4e96cdccd422b3b64707d225f7d09c6
Yiteng Zhang <ytzhang89@gmail.com>
2014-03-07 12:23:05 +0000

    Test 12
    Back to orginal

--- connection.php
+++ connection.php
@@ -1,8 +1,10 @@
 <?php
 // Connect to server and select databse.
-//mysql_connect("eu-cdbr-azure-west-b.cloudapp.net", "be1a7d8c1e05ec", "9b94feea")or die("cannot connect"); 
-//mysql_select_db("gamehuba9mv7nxeb")or die("cannot select DB");
+mysql_connect("eu-cdbr-azure-west-b.cloudapp.net", "be1a7d8c1e05ec", "9b94feea")or die("cannot connect"); 
+mysql_select_db("gamehuba9mv7nxeb")or die("cannot select DB");
 
+/*
+//Code from Azure php mysql tutorial
 // DB connection info
 $host = "eu-cdbr-azure-west-b.cloudapp.net";
 $user = "be1a7d8c1e05ec";
@@ -16,5 +18,7 @@ try {
 catch(Exception $e){
     die(var_dump($e));
 }
+
+*/
 
 ?>
