babb9499d4dbe950ebe7d3012f66118481bef34d
Yiteng Zhang <ytzhang89@gmail.com>
2014-03-14 15:11:29 +0000

    Test 20

--- connection.php
+++ connection.php
@@ -1,24 +1,5 @@
 <?php
 // Connect to server and select databse.
-mysql_connect("eu-cdbr-azure-west-b.cloudapp.net", "be1a7d8c1e05ec", "9b94feea")or die("cannot connect"); 
-mysql_select_db("gamehuba9mv7nxeb")or die("cannot select DB");
-
-/*
-//Code from Azure php mysql tutorial
-// DB connection info
-$host = "eu-cdbr-azure-west-b.cloudapp.net";
-$user = "be1a7d8c1e05ec";
-$pwd = "9b94feea";
-$db = "gamehuba9mv7nxeb";
-// Connect to database.
-try {
-    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(var_dump($e));
-}
-
-*/
-
+mysql_connect("localhost", "arsenalchem", "thisisarsenal")or die("cannot connect"); 
+mysql_select_db("game_database")or die("cannot select DB");
 ?>
